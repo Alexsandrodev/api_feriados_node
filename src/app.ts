b075ebeccfbe213afbb70c818e9a99f6bf4d9d2a
Alexsandro Mendes <alexsandroferreiramendes00@gmail.com>
2025-01-31 02:43:24 -0300

    feat(app): adição da biblioteca cors, e alteração na url

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,19 @@
 import express from "express";
+import cors from 'cors'
 import feriadosRoutes from "./routes/feriadosRoutes";
 
 const app = express();
 
+app.use(cors({
+    origin: ["http://localhost:8000", "http://127.0.0.1:8000"],
+    methods: ['GET', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+
+}))
+
 app.use(express.json());
 
-app.use('/feriados', feriadosRoutes)
+app.use(feriadosRoutes)
 
 export default app
